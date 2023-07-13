79a552a9b46785447036fec8d02e56875fa7fef9
itc-n23026 <n23026@std.it-college.ac.jp>
2023-07-13 07:44:19 +0000

    Chapter3 3.6

--- CHAPTER03/Q3_6_1.py
+++ CHAPTER03/Q3_6_1.py
@@ -1,12 +1,12 @@
 import random
 
-numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+numbers = list(map(str, range(10)))
 sample4 = random.sample(numbers, k=4)
 num4 = "".join(sample4)
-while True:
-    val = input()
-    if val == num4:
-        print("OK")
-        break
-    else:
-        print(val, ": NG")
+
+val = "".join(random.sample(numbers, k=4))
+while val != num4:
+    print(val, ": NG")
+    val = "".join(random.sample(numbers, k=4))
+
+print(val, ": OK")
