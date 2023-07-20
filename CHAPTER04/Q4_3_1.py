5f7d644dc593f5e61a37914297fd54f0a7f71306
itc-n23026 <n23026@std.it-college.ac.jp>
2023-07-20 06:58:18 +0000

    Chapter4 4.3

--- CHAPTER04/Q4_3_1.py
+++ CHAPTER04/Q4_3_1.py
@@ -4,3 +4,14 @@ def concat_words(*args, separator=","):
 
 print(concat_words("a", "b", "c", "d", separator="_"))
 print(concat_words("4_choume", "Minatoku", "Tokyo", "Japan", separator=" "))
+print(concat_words("沖縄県", "那覇市", "樋川", "2丁目", "16-18", separator=" "))
+print(
+    concat_words(
+        "home",
+        "vagrant",
+        "programming1_git",
+        "ProgrammingI",
+        "CHAPTER04",
+        separator="/",
+    )
+)
