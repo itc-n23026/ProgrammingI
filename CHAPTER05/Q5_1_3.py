0286525ac2b74dcc748ef28b54b7ed67804ec0b1
itc-n23026 <n23026@std.it-college.ac.jp>
2023-08-08 05:53:34 +0000

    Chapter5 5.1

--- CHAPTER05/Q5_1_3.py
+++ CHAPTER05/Q5_1_3.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 queue = []
 queue.append(1)
 print("queue:", queue)
@@ -5,4 +7,12 @@ queue.append(2)
 print("queue:", queue)
 print("pop from queue 1st value:", queue.pop(0))
 print("pop from queue 2nd value:", queue.pop(0))
+print("queue:", queue)
+
+queue = deque([1, 2])
+queue.append(3)
+queue.append(4)
+print("queue:", queue)
+print("popleft from queue 1st value:", queue.popleft())
+print("popleft from queue 2nd value:", queue.popleft())
 print("queue:", queue)
